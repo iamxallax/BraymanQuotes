2bb18ed75059da03c9f9a7381793528957895e15
Aaron Hoffman <aaron_hoffman@fastmail.com>
2025-03-04 09:46:39 -0800

    functioning centering and quote picking

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, g, render_template
+import time
 
 app = Flask(__name__)
 
+@app.before_request
+def load_data():
+    with open('quotes.txt') as f:
+        g.quotes = f.readlines()
+
 @app.route('/')
 def home_page():
-    return '<p>hello</p>'
+    curtime = time.gmtime()
+    day = curtime.tm_yday
+    index = day % len(g.quotes)
+    quote = g.quotes[index]
+
+    return render_template('page.html', quote=quote)
